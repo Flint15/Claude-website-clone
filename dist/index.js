620877124419dc90556e53f2ea4e1f8cab5d7ea8
Flint15 <chesspace341@gmail.com>
2025-12-18 00:18:00 +0100

    Add disabled and enabled states to the send button

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,20 @@ if (sidebarButton) {
         sidebarElement === null || sidebarElement === void 0 ? void 0 : sidebarElement.classList.toggle('closed');
     });
 }
+const inputElement = document.querySelector('#input-element');
+const sendButton = document.querySelector('.send-button');
+const sendSVG = document.querySelector('.arrow-icon path');
+inputElement === null || inputElement === void 0 ? void 0 : inputElement.addEventListener('input', () => {
+    if (inputElement.value && !(sendButton === null || sendButton === void 0 ? void 0 : sendButton.classList.contains('active'))) {
+        sendButton === null || sendButton === void 0 ? void 0 : sendButton.classList.add('active');
+        if (sendSVG)
+            sendSVG.style.fill = '#ffffffff';
+    }
+    else if (!inputElement.value) {
+        sendButton === null || sendButton === void 0 ? void 0 : sendButton.classList.remove('active');
+        if (sendSVG)
+            sendSVG.style.fill = '#9C9A92';
+    }
+});
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,17 @@ if (sidebarButton) {
     sidebarElement?.classList.toggle('closed')
   })
 }
+
+const inputElement = document.querySelector<HTMLInputElement>('#input-element')
+const sendButton = document.querySelector('.send-button')
+const sendSVG = document.querySelector<SVGPathElement>('.arrow-icon path')
+
+inputElement?.addEventListener('input', () => {
+  if (inputElement.value && !sendButton?.classList.contains('active')) {
+    sendButton?.classList.add('active')
+    if (sendSVG) sendSVG.style.fill = '#ffffffff'
+  } else if (!inputElement.value) {
+    sendButton?.classList.remove('active')
+    if (sendSVG) sendSVG.style.fill = '#9C9A92'
+  }
+})
